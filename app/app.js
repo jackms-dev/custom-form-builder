48dfa953e1a241d3077e00969a90ca1df89c72ca
Jack MS <jmcdermottsweeney@gmail.com>
2025-08-04 18:52:03 -0500

    Form question types

--- app/app.js
+++ app/app.js
@@ -8,22 +8,42 @@ class Form {
     this.name = isValidString(name) ? name : "New Form";
     this.questions = normalizeToArray(questions);
   }
-  addQuestion(question) {}
-  removeQuestion(question) {}
-  hasQuestion(question) {}
-  findQuestion(question) {}
+  addQuestions(questions) {
+    normalizeToArray(questions).forEach((e) => {
+      if (e instanceof FormQuestion) this.questions.push(e);
+    });
+  }
+  removeQuestions(questions) {
+    normalizeToArray(questions).forEach((e) => {
+      const hasQuestion = this.questions.find((q) => q == e);
+      if (hasQuestion)
+        this.questions.splice(this.indexOfQuestion(hasQuestion), 1);
+    });
+  }
+  questionAtIndex(index) {
+    if (typeof index === "number") return this.questions.at(index);
+  }
+  indexOfQuestion(question) {
+    return this.questions.indexOf(question);
+  }
   toJSON() {}
 }
 
 // Use this to define accepted form question types
 function isValidType(type) {
-  const formQuestionTypes = ["text", "single select", "multiple select"];
+  const formQuestionTypes = [
+    "text",
+    "select",
+    "single-select",
+    "multiple-select",
+  ];
   return formQuestionTypes.some((e) => e == type);
 }
 
 class FormQuestion {
-  constructor({ question, type, required }) {
+  constructor({ question, value = null, type, required }) {
     this.question = isValidString(question) ? question : "Untitled";
+    this.value = value;
     this.type = isValidType(type) ? type : null;
     this.required = typeof required === "boolean" ? required : false;
   }
@@ -47,41 +67,123 @@ class FormQuestion {
   }
 }
 
+// Subclasses should decide what the value for FormQuestion.value can be and how to set it.
 class FormQuestionText extends FormQuestion {
   constructor(props) {
     super({ ...props, type: "text" });
-    if (isValidString(props?.text)) this.text = props.text;
-    if (isValidString(props?.value)) this.value = props.value;
+    this.value = props?.value ? props.value : "";
   }
-  setText(text) {}
-  setValue(value) {}
+  setValue(value) {
+    if (isValidString(value)) this.value = value;
+  }
+  clearValue() {
+    this.value = "";
+  }
 }
 
 class FormQuestionOptions {
-  constructor({ options = [], value = [], multiple }) {
-    this.multiple = typeof multiple === "boolean" ? multiple : false;
-    this.options = options.filter((e) => isValidString(e));
-    if (this.multiple) {
+  constructor({ options = [], multiple }) {
+    this.options = options.filter((opt) => isValidString(opt));
+    // this.multiple = typeof multiple === "boolean" ? multiple : false;
+  }
+  addOptions(options) {
+    normalizeToArray(options)
+      .filter((opt) => isValidString(opt))
+      .forEach((opt) => {
+        if (!this.hasOption(opt)) {
+          this.options.push(opt);
+        }
+      });
+  }
+  removeOptions(option) {
+    normalizeToArray(option)
+      .filter((opt) => isValidString(opt))
+      .forEach((opt) => {
+        for (const j of this.options) {
+          if (j == opt) {
+            this.options.splice(this.options.indexOf(j), 1);
+          }
+        }
+      });
+  }
+  getOption(option) {
+    return this.options.find((e) => e == option);
+  }
+  hasOption(option) {
+    return this.options.some((e) => e == option);
+  }
+  clearOptions() {
+    for (const j of this.options) {
+      this.options.splice(this.options.indexOf(j), 1);
     }
   }
 }
 
-// Figure out how to extend FormQuestionOptions in addition to FormQuestion here
-// https://medium.com/@thevirtuoid/extending-multiple-classes-in-javascript-2f4752574e65
-class FormQuestionSingleSelect extends FormQuestion {
+class FormQuestionSelect extends FormQuestion {
   constructor(props) {
-    super({ ...props, type: "single select" });
-    if (Array.isArray(props?.options))
-      this.options = options.filter((e) => isValidString(e));
-    if (this.options.some((e) => props?.value == e)) this.value = props.value;
+    super({ ...props, type: "select" });
+    this.type = "select";
+    this.options =
+      props?.options instanceof FormQuestionOptions
+        ? props.options
+        : new FormQuestionOptions({
+            options: props?.options || [],
+            multiple: props?.multiple || false,
+          });
+    this.value = Array.isArray(props?.value)
+      ? props.value.filter((element) =>
+          this.options.find((opt) => element == opt)
+        )
+      : [];
+    this.multiple = props?.multiple ?? false;
   }
-  addOption(option) {}
-  removeOption(option) {}
+  setValues(value) {
+    normalizeToArray(value).forEach((e) => {
+      if (this.options.find((opt) => e == opt))
+        multiple ? this.value.push(value) : this.value.splice(0, 1, value);
+    });
+  }
+  removeValues(value) {
+    normalizeToArray(value).forEach((e) => {
+      if (e == value) this.value.splice(this.value.indexOf(j), 1);
+    });
+  }
+  clearValues() {
+    for (const j of this.value) {
+      this.value.splice(this.value.indexOf(j), 1);
+    }
+  }
 }
 
-class FormQuestionMultipleSelect extends FormQuestion {
-  constructor(props) {
-    super({ ...props, type: "multiple select" });
-    this.options = options.filter((e) => isValidString(e));
-  }
-}
+const form = new Form("My form", []);
+
+form.addQuestions([
+  new FormQuestionText({
+    question: "What is your name?",
+    value: null,
+    type: "text",
+    required: true,
+  }),
+  new FormQuestionSelect({
+    question: "What are your favorite colors? Select multiple.",
+    value: null,
+    type: "select",
+    required: true,
+    options: new FormQuestionOptions({
+      options: ["red", "orange", "yellow", "green", "blue", "purple"],
+      multiple: true,
+    }),
+  }),
+  new FormQuestionSelect({
+    question: "What is your favorite animal?",
+    value: null,
+    type: "select",
+    required: false,
+    options: new FormQuestionOptions({
+      options: ["dog", "cat", "fish"],
+      multiple: false,
+    }),
+  }),
+]);
+
+console.log(form.questions[2].options);
